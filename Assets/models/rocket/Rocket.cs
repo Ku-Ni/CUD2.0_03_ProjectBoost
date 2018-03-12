59014872b6ff166b32cf4ec82f19051fa7b561da
Coenie <coenie.basson@gmail.com>
2018-03-12 23:08:10 +0000

    Adding movement

--- Assets/models/rocket/Rocket.cs
+++ Assets/models/rocket/Rocket.cs
@@ -1,16 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Rocket : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Rigidbody rigidBody;
+
+    private readonly KeyCode thrust = KeyCode.Space;
+    private readonly KeyCode left = KeyCode.LeftArrow;
+    private readonly KeyCode right = KeyCode.RightArrow;
+
+    // Use this for initialization
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        HandleMovement();
+    }
+
+
+    private void HandleMovement() {
+
+
+        if (Input.GetKey(thrust)) {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(left) ^ Input.GetKey(right)) {
+            if (Input.GetKey(right)) {
+                Debug.Log("Rotate right");
+                transform.Rotate(Vector3.forward);
+            }
+            else if (Input.GetKey(left)) {
+                Debug.Log("Rotate left");
+                transform.Rotate(Vector3.back);
+            }
+        }
+
+    }
 }
